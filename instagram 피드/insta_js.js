3552f5e6957e547fe8e4ccd38fd48df2e39b530a
hyeonji00 <50583561+hyeonji00@users.noreply.github.com>
2021-04-18 15:51:21 +0900

    version_1

---
+++ instagram 피드/insta_js.js
@@ -0,0 +1,27 @@
+let heart=document.querySelector("#heart");
+
+heart.onclick=changepic;
+
+let cnt=0;
+
+function changepic(){
+    if(cnt%2==0){
+        heart.src="하트(누름).jpg";
+    }
+    else{
+        heart.src="하트(누름x).jpg";
+    }
+    cnt++;
+}
+
+const open = () => {
+    document.querySelector(".modal").classList.remove("hidden");
+  }
+
+const close = () => {
+    document.querySelector(".modal").classList.add("hidden");
+  }
+
+  document.querySelector("#modal").addEventListener("click", open);
+  document.querySelector("#close").addEventListener("click", close);
+  document.querySelector(".bg").addEventListener("click", close);
