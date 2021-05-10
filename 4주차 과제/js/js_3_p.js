a55854be06060dbdd7dade2c64ea58c267adc000
hyeonji00 <50583561+hyeonji00@users.noreply.github.com>
2021-05-10 17:38:11 +0900

    version_1

---
+++ 4주차 과제/js/js_3_p.js
@@ -0,0 +1,32 @@
+const open_1 = () => {
+    document.querySelector(".modalBox_1").classList.remove("hidden");
+  }
+
+const close_1 = () => {
+    document.querySelector(".modalBox_1").classList.add("hidden");
+  }
+
+  document.querySelector("#modal_1").addEventListener("click", open_1);
+  document.querySelector(".bg_1").addEventListener("click", close_1);
+
+const open_2 = () => {
+    document.querySelector(".modalBox_2").classList.remove("hidden");
+  }
+
+const close_2 = () => {
+    document.querySelector(".modalBox_2").classList.add("hidden");
+  }
+
+  document.querySelector("#modal_2").addEventListener("click", open_2);
+  document.querySelector(".bg_2").addEventListener("click", close_2);
+
+  const open_3 = () => {
+    document.querySelector(".modalBox_3").classList.remove("hidden");
+  }
+
+const close_3 = () => {
+    document.querySelector(".modalBox_3").classList.add("hidden");
+  }
+
+  document.querySelector("#modal_3").addEventListener("click", open_3);
+  document.querySelector(".bg_3").addEventListener("click", close_3);
