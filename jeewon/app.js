5e9f94822df86c8fb5491bd992edabdb28474d16
jeewonMoon <papers1210@kakao.com>
2021-05-10 15:52:50 +0900

    5

---
+++ jeewon/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var static = require('serve-static');
+
+var app = express();
+app.use(static(path.join(__dirname, '/')));
+
+app.set('port', process.env.PORT || 3000);
+app.get('/', function(req, res){
+    res.redirect('index.html');
+});
+
+http.createServer(app).listen(app.get('port'),function(){
+    console.log('Server START...' + app.get('port'));
+});
