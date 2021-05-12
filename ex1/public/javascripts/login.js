834e128e612fafdecb9a3f7f5a803f9202e57a16
박현우 <gusdn3477@naver.com>
2021-05-12 14:50:02 +0900

    과제1 flex 방식으로 수정

--- ex1/public/javascripts/login.js
+++ ex1/public/javascripts/login.js
@@ -1,77 +1,82 @@
-let photos = ['광화문.jpg', 'hanriver.jpg', 'instagram.png', 'hanriver.jpg'];
-let a = ''
-for (let i = 0; i < photos.length; i++) {
-    a += `<img id="image${i}" onclick="clickPhoto(${i});" src="/images/feed/${photos[i]}" style="width : 280.4px; height : 280.4px; margin-top : 1px; margin-left : 3px; cursor : pointer;">`;
-    if (i % 3 == 2) {
-        a += `<br>`;
+let idPattern = /^[A-Za-z]{1}[A-Za-z0-9]{3,19}$/;
+let pwPattern = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+let emailPattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+let phonePattern = /(^02.{0}|^01.{1}|[0-9]{3})([0-9]+)([0-9]{4})/g;
+let flag = 0, flag2 = 0, flag3 = 0, flag4 = 0;
+let imgArray = new Array();
+imgArray[0] = "/images/login/phone.JPG";
+imgArray[1] = "/images/login/phone2.JPG";
+imgArray[2] = "/images/login/phone3.JPG";
+let i = 0;
+function showImage() {
+    var imgNum = i;
+    var objImg = document.getElementById("introImg");
+    objImg.src = imgArray[imgNum % 3];
+    setTimeout("showImage()", 5000);
+    i += 1;
+    console.log(i);
+    if (i >= 3) {
+        i = 0;
     }
 }
 
-function clickPhoto(k) {
+let number_check = document.getElementById("number").addEventListener('blur', function () {
 
-    let a = '';
-    a += `<div class="child_content"><img src="/images/feed/${photos[k]}" style="width : 150.4px; height : 50.4px;</div>
-            <div class="child_content" id="change">모달 준비중</div>
-            <div id="modal_close_btn2" class="child_content">취소</div>`;
-    document.getElementById('my_modal_child2').innerHTML = a;
+    if (document.getElementById("number").value == '') {
+        flag = 0;
+    }
 
-    document.getElementById('image' + k).addEventListener('click', function () {
-        let q = document.getElementById('my_modal2');
-        q.style.display = 'block';
-    });
+    else if (emailPattern.test(document.getElementById('number').value) || phonePattern.test(document.getElementById('number').value)) {
+        flag = 1;
+    }
 
-    document.getElementById('modal_close_btn2').addEventListener('click', function () {
-        document.getElementById('my_modal2').style.display = 'none';
-    });
+    else if (!(emailPattern.test(document.getElementById('number').value) || phonePattern.test(document.getElementById('number').value))) {
+        flag = 0;
+    }
+})
 
-}
+let password_check = document.getElementById("password").addEventListener('blur', function () {
 
-document.getElementById('inner').innerHTML = a;
-document.getElementById('input_control').addEventListener('focus', function () {
+    if (document.getElementById("password").value == '') {
+        flag2 = 0;
+    }
 
-    if (document.getElementById('input_control').value == '') {
-        document.getElementById('search').style.display = 'block';
-        document.getElementById('search').style.marginLeft = '15px';
+    else if (pwPattern.test(document.getElementById('password').value)) {
+        flag2 = 1;
     }
 
     else {
-        document.getElementById('search').style.display = 'none';
+        flag2 = 0;
     }
-})
-
-document.getElementById('input_control').addEventListener('blur', function () {
-
-    if (document.getElementById('input_control').value == '') {
-        document.getElementById('search').style.display = 'block';
-        document.getElementById('search').style.marginLeft = "90px";
-    }
-
-    else {
-        document.getElementById('search').style.display = 'none';
-    }
-})
-
-document.getElementById('input_control').addEventListener('keydown', function () {
-    document.getElementById('search').style.display = 'none';
-})
-
-document.getElementById('home').addEventListener('click', function () {
-    location.href = "/feed/";
-})
-
-document.getElementById('home2').addEventListener('click', function () {
-    location.href = "/feed/";
-})
-
-document.getElementById('person').addEventListener('click', function () {
-    location.href = "/mypage/";
-})
-
-document.getElementById('modal_btn').addEventListener('click', function () {
-    let q = document.getElementById('my_modal');
-    q.style.display = 'block';
 });
 
-document.getElementById('modal_close_btn').addEventListener('click', function () {
-    document.getElementById('my_modal').style.display = 'none';
+let join = document.getElementById("join").addEventListener('click', function () {
+
+    if ((flag + flag2) == 2) {
+        alert('로그인 성공!');
+        location.href = "/feed/";
+
+    }
+});
+
+let join2 = document.getElementById("number").addEventListener('blur', function () {
+
+    if ((flag + flag2) == 2) {
+        document.getElementById('join').style.opacity = 1;
+    }
+
+    else {
+        document.getElementById('join').style.opacity = 0.5;
+    }
+});
+
+let join3 = document.getElementById("password").addEventListener('blur', function () {
+
+    if ((flag + flag2) == 2) {
+        document.getElementById('join').style.opacity = 1;
+    }
+
+    else {
+        document.getElementById('join').style.opacity = 0.5;
+    }
 });
